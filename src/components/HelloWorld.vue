3360b0ddaef3ccee1d9da55296add84574b96b61
M. Gambill <46201338+mgambill@users.noreply.github.com>
2019-11-07 18:06:07 -0500

    minor tweak

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,7 +5,7 @@
     | For a guide and recipes on how to configure / customize this project,
     br
     |       check out the
-    a(href='https://cli.vuejs.org' target='_blank' rel='noopener') vue-cli documentation
+    a(href='https://cli.vuejs.org' target='_blank' rel='noopener')  vue-cli documentation
     | .
   h3 Installed CLI Plugins
   ul
